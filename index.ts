44c0f1ec5f0edef2f67deec0f53df943424ad0a2
Mike Barkmin <mike@barkmin.eu>
2023-04-28 21:45:36 +0200

    remove gcloud dependency

--- index.ts
+++ index.ts
@@ -1,38 +1,25 @@
-import { Storage } from "@google-cloud/storage";
+import fs from "fs/promises";
 import cors from "cors";
 import express from "express";
 import { nanoid } from "nanoid";
 import favicon from "serve-favicon";
 import * as path from "path";
 
-const PROJECT_NAME = process.env.GOOGLE_CLOUD_PROJECT || "excalidraw-json-dev";
-const PROD = PROJECT_NAME === "excalidraw-json";
 const LOCAL = process.env.NODE_ENV !== "production";
-const BUCKET_NAME = PROD
-  ? "excalidraw-json.appspot.com"
-  : "excalidraw-json-dev.appspot.com";
+const FOLDER_NAME =
+  process.env.FOLDER_NAME || path.join(__dirname, "json-store");
+const FILE_SIZE_LIMIT = "2MB";
 
-const FILE_SIZE_LIMIT = 2 * 1024 * 1024;
-const storage = new Storage(
-  LOCAL
-    ? {
-        projectId: PROJECT_NAME,
-        keyFilename: `${__dirname}/keys/${PROJECT_NAME}.json`,
-      }
-    : undefined
-);
-
-const bucket = storage.bucket(BUCKET_NAME);
 const app = express();
+app.use(express.json({ limit: FILE_SIZE_LIMIT }));
 
 let allowOrigins = [
-  "excalidraw.vercel.app",
-  "https://dai-shi.github.io",
-  "https://excalidraw.com",
-  "https://www.excalidraw.com",
-  "https://math.preview.excalidraw.com",
+  "openpatch.vercel.app",
+  "https://onlineide.openpatch.com",
+  "https://nrw.onlineide.openpatch.com",
+  "https://sqlide.openpatch.com",
 ];
-if (!PROD) {
+if (!LOCAL) {
   allowOrigins.push("http://localhost:");
 }
 
@@ -57,51 +44,27 @@ app.get("/", (req, res) => res.sendFile(`${process.cwd()}/index.html`));
 app.get("/api/v2/:key", corsGet, async (req, res) => {
   try {
     const key = req.params.key;
-    const file = bucket.file(key);
-    await file.getMetadata();
     res.status(200);
     res.setHeader("content-type", "application/octet-stream");
-    file.createReadStream().pipe(res);
+    res.sendFile(path.join(FOLDER_NAME, key + ".json"));
   } catch (error) {
     console.error(error);
     res.status(404).json({ message: "Could not find the file." });
   }
 });
 
-app.post("/api/v2/post/", corsPost, (req, res) => {
+app.post("/api/v2/post/", corsPost, async (req, res) => {
   try {
-    let fileSize = 0;
     const id = nanoid();
-    const blob = bucket.file(id);
-    const blobStream = blob.createWriteStream({ resumable: false });
+    const content = req.body;
+    await fs.writeFile(
+      path.join(FOLDER_NAME, id + ".json"),
+      JSON.stringify(content)
+    );
 
-    blobStream.on("error", (error) => {
-      console.error(error);
-      res.status(500).json({ message: error.message });
-    });
-
-    blobStream.on("finish", async () => {
-      res.status(200).json({
-        id,
-        data: `${LOCAL ? "http" : "https"}://${req.get("host")}/api/v2/${id}`,
-      });
-    });
-
-    req.on("data", (chunk) => {
-      blobStream.write(chunk);
-      fileSize += chunk.length;
-      if (fileSize > FILE_SIZE_LIMIT) {
-        const error = {
-          message: "Data is too large.",
-          max_limit: FILE_SIZE_LIMIT,
-        };
-        blobStream.destroy();
-        console.error(error);
-        return res.status(413).json(error);
-      }
-    });
-    req.on("end", () => {
-      blobStream.end();
+    res.status(200).json({
+      id,
+      data: `${LOCAL ? "http" : "https"}://${req.get("host")}/api/v2/${id}`,
     });
   } catch (error) {
     console.error(error);
@@ -109,5 +72,14 @@ app.post("/api/v2/post/", corsPost, (req, res) => {
   }
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`http://localhost:${port}`));
+async function start() {
+  await fs.mkdir(FOLDER_NAME).catch((error) => {
+    if (error.code !== "EEXIST") {
+      throw error;
+    }
+  });
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => console.log(`http://localhost:${port}`));
+}
+
+start();
